766877068cb102b7cea4d0ee5f399ee97e9a407e
jspe2ba12 <jspe2ba12@scs.local>
2025-04-25 10:43:16 +0800

    Initial Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,7 +7,51 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  time = 25;
+  seconds = 59;
 
+
+
+  breakTime = 5;
+  isBreak = false;
+  isRunning = true;
+  isPaused = true;
+  isStarted = false;
+  
   constructor() {}
+
+  startTimer(){
+    // let timer = setInterval(() => {
+    //   this.time--;
+    //   if(this.time <= 0){
+    //     clearInterval(timer);
+    //     alert("Time's up!");
+    //     console.log("Clock is running");
+    //   }
+    // }, 1000);
+    
+    this.time--;
+    this.seconds--;
+
+
+
+    if(this.isRunning){
+        this.isRunning = true;
+        this.isStarted = true;
+        console.log("Clock is running");
+      return;
+    }else{  
+      this.isRunning = false;
+      this.isStarted = false;
+      console.log("Clock is not running");
+    }
+  }
+
+
+
+
+  
+
+
 
 }
